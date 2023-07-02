f2cf92f1335647678551baf9528d29e61cbfd0e6
anton-avramchuk <aav1984@gmail.com>
2023-07-02 15:21:19 +0300

    tests

--- src/tests/Zeus.Core.Tests/Reflection/AssemblyFinderTests.cs
+++ src/tests/Zeus.Core.Tests/Reflection/AssemblyFinderTests.cs
@@ -7,18 +7,6 @@ namespace Zeus.Core.Tests.Reflection
     [TestFixture]
     public class AssemblyFinderTests
     {
-        private Mock<IApplicationModuleContainer> _applicationModuleContainerMock;
-
-        [SetUp]
-        public void SetUp()
-        {
-            _applicationModuleContainerMock = new Mock<IApplicationModuleContainer>();
-        }
-
-        private AssemblyFinder Create()
-        {
-            return new AssemblyFinder(_applicationModuleContainerMock.Object);
-        }
 
         [Theory]
         [TestCase(new object[] { new Type[] { } })]
